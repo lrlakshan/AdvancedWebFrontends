f2b285f3c7cc708f6e1c1ef16bcdb2bae4e3172f
Lakshan Rathnayaka <lrlakshan@gmail.com>
2024-01-15 12:00:09 +0200

    add and delete players

--- elm/players_CRUD/src/Main.elm
+++ elm/players_CRUD/src/Main.elm
@@ -1,8 +1,8 @@
 module Main exposing (..)
 
 import Browser
-import Html exposing (..)
-import Html.Attributes exposing (..)
+import Html exposing (Html, div, h1, input, button, form, label, text, ol, li)
+import Html.Attributes exposing (id, type_, placeholder, class)
 import Html.Events exposing (onCheck, onClick, onInput, onSubmit)
 
 
@@ -42,21 +42,51 @@ update : Msg -> Model -> Model
 update msg model =
     case msg of
         SetName name ->
-            model
+            { model | newPlayer = setPlayerName name model.newPlayer }
 
         AddPlayer ->
-            model
+            { model | players = model.players ++ [model.newPlayer], newPlayer = initPlayer (List.length model.players + 1) }
 
         DeletePlayer id ->
-            model
+            { model | players = List.filter (\player -> player.id /= id) model.players }
 
         ModifyPlayer id status ->
-            model
+            { model | players = List.map (\player -> if player.id == id then { player | isActive = status } else player) model.players }
+
+setPlayerName : String -> Player -> Player
+setPlayerName name player =
+    { player | name = name }
 
 
 view : Model -> Html Msg
 view model =
-    h1 [] [ text "Elm Exercise: Players CRUD" ]
+    div []
+        [ h1 [] [ text "Elm Exercise: Players CRUD" ]
+        , viewForm
+        , viewPlayers model.players
+        ]
+
+viewForm : Html Msg
+viewForm =
+    form [ id "submit-player", onSubmit AddPlayer ]
+        [ input [ id "input-player", type_ "text", placeholder "Enter player name", onInput SetName ] []
+        , button [ id "btn-add", type_ "submit" ] [ text "Add Player" ]
+        ]
+
+viewPlayers : List Player -> Html Msg
+viewPlayers players =
+    ol [ id "players-list" ] (List.map viewPlayer players)
+
+viewPlayer : Player -> Html Msg
+viewPlayer player =
+    li []
+        [ div [ class "player-name" ] [ text player.name ]
+        , label [ class "player-status" ]
+            [ input [ type_ "checkbox", class "player-status", onCheck (\isChecked -> ModifyPlayer player.id isChecked) ] []
+            , text "Active"
+            ]
+        , button [ class "btn-delete", onClick (DeletePlayer player.id) ] [ text "Delete" ]
+        ]
 
 
 main : Program () Model Msg
