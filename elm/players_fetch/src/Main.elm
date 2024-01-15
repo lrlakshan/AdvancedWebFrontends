5009e26b2e1a20e9fb0790f07cd68cfb10464917
Lakshan Rathnayaka <lrlakshan@gmail.com>
2024-01-15 14:15:23 +0200

    fetching players from BE

--- elm/players_fetch/src/Main.elm
+++ elm/players_fetch/src/Main.elm
@@ -6,8 +6,8 @@
 module Main exposing (..)
 
 import Browser
-import Html exposing (..)
-import Html.Attributes exposing (..)
+import Html exposing (Html, div, h1, input, button, form, label, text, ol, li)
+import Html.Attributes exposing (id, type_, placeholder, class)
 import Html.Events exposing (onCheck, onClick, onInput, onSubmit)
 import Http
 import Json.Decode as Decode exposing (Decoder, field, map3)
@@ -46,9 +46,11 @@ playersDecoder =
 
 
 fetchPlayers : String -> Cmd Msg
-fetchPlayers url = Cmd.none
-
-
+fetchPlayers url =
+    Http.get
+        { url = url
+        , expect = Http.expectJson FetchPlayers playersDecoder
+        }
 
 listLast : List a -> Maybe a
 listLast list =
@@ -71,29 +73,74 @@ init _ =
     )
 
 
-update : Msg -> Model -> ( Model, Cmd Msg )
+update : Msg -> Model -> (Model, Cmd Msg)
 update msg model =
     case msg of
-        SetName word ->
-            ( model, Cmd.none )
+        SetName name ->
+            ({ model | newPlayer = setNameIn name model.newPlayer }, Cmd.none)
 
         AddPlayer ->
-            ( model, Cmd.none )
+            ({ model | players = model.players ++ [model.newPlayer], newPlayer = initPlayer (List.length model.players + 1) }, Cmd.none)
 
         DeletePlayer id ->
-            ( model, Cmd.none )
+            ({ model | players = List.filter (\player -> player.id /= id) model.players }, Cmd.none)
 
         ModifyPlayer id status ->
-            ( model, Cmd.none )
+            ({ model | players = List.map (\player -> if player.id == id then { player | isActive = status } else player) model.players }, Cmd.none)
 
         FetchPlayers data ->
-            ( model, Cmd.none )
+            case data of
+                Ok fetchedPlayers ->
+                    ({ model | players = fetchedPlayers, reqStatus = "" }, Cmd.none)
+
+                Err err ->
+                     ({ model | reqStatus = "An error has occurred!!!" }, Cmd.none)
+
+viewRequestStatus : String -> Html Msg
+viewRequestStatus status =
+    div [ id "request-status" ] [ text status ]
+
+setNameIn : String -> Player -> Player
+setNameIn name player =
+    { player | name = name }
 
 
 view : Model -> Html Msg
 view model =
-    h1 [] [ text "Elm Exercise: Players Fetch" ]
+    div []
+        [ h1 [] [ text "Elm Exercise: Players CRUD" ]
+        , viewForm
+        , viewPlayers model.players
+        , viewRequestStatus model.reqStatus -- Display request status
+        ]
 
+viewForm : Html Msg
+viewForm =
+    form [ id "submit-player", onSubmit AddPlayer ]
+        [ input [ id "input-player", type_ "text", placeholder "Enter player name", onInput SetName ] []
+        , button [ id "btn-add", type_ "submit" ] [ text "Add Player" ]
+        ]
+
+viewPlayers : List Player -> Html Msg
+viewPlayers players =
+    ol [ id "players-list" ] (List.map viewPlayer players)
+
+viewPlayer : Player -> Html Msg
+viewPlayer player =
+    li [id <| "player-" ++ String.fromInt player.id]
+        [ div [class "player-name"] [text player.name]
+        , label [class "player-status"]
+            [ input
+                [ type_ "checkbox"
+                , class "player-status"
+                , onCheck (\isChecked -> ModifyPlayer player.id isChecked)
+                , Html.Attributes.checked player.isActive -- Set the checked property based on isActive
+                ]
+                []
+            , text "Active"
+            ]
+        , button [class "btn-delete", onClick (DeletePlayer player.id)] [text "Delete"]
+        ]
 
 main : Program () Model Msg
 main =
