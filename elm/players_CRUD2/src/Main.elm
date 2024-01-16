904cc2b45c427164c31ad07b863a94a0868e76b4
Lakshan Rathnayaka <lrlakshan@gmail.com>
2024-01-16 09:19:43 +0200

    POST, PUT, DELETE integration with frontend

--- elm/players_CRUD2/src/Main.elm
+++ elm/players_CRUD2/src/Main.elm
@@ -8,8 +8,8 @@
 module Main exposing (..)
 
 import Browser
-import Html exposing (..)
-import Html.Attributes exposing (..)
+import Html exposing (Html, div, h1, input, button, form, label, text, ol, li)
+import Html.Attributes exposing (id, type_, placeholder, class)
 import Html.Events exposing (onCheck, onClick, onInput, onSubmit)
 import Http
 import Json.Decode as Decode exposing (Decoder, field, map3)
@@ -62,18 +62,45 @@ playersDecoder =
 
 
 fetchPlayers : String -> Cmd Msg
-fetchPlayers url = Cmd.none
+fetchPlayers url =
+    Http.get
+        { url = url
+        , expect = Http.expectJson FetchPlayers playersDecoder
+        }
 
 postPlayerReq : String -> Player -> Cmd Msg
-postPlayerReq url player = Cmd.none
+postPlayerReq url player =
+    Http.post
+        { url = url
+        , body = Http.jsonBody <| playerEncoder player
+        , expect = Http.expectJson AddPlayer playerDecoder
+        }
 
 
 deletePlayerReq : String -> Int -> Cmd Msg
-deletePlayerReq url id = Cmd.none
+deletePlayerReq url id =
+    Http.request
+        { method = "DELETE"
+        , headers = []
+        , url = url ++ String.fromInt id
+        , body = Http.emptyBody
+        , expect = Http.expectWhatever (DeletePlayer id)
+        , timeout = Nothing
+        , tracker = Nothing
+        }
 
 
 putPlayerReq : String -> Player -> Cmd Msg
-putPlayerReq url player = Cmd.none
+putPlayerReq url player =
+    Http.request
+        { method = "PUT"
+        , headers = []
+        , url = url
+        , body = Http.jsonBody <| playerEncoder player
+        , expect = Http.expectJson ModifyPlayer playerDecoder
+        , timeout = Nothing
+        , tracker = Nothing
+        }
 
 
 listLast : List a -> Maybe a
@@ -89,7 +116,7 @@ initPlayer id =
 initModel : Model
 initModel =
     { players = []
-    , newPlayer = initPlayer 0
+    , newPlayer = initPlayer 1
     , baseUrl = "http://localhost:3001/api/players/"
     , reqStatus = "Loading..."
     }
@@ -106,33 +133,126 @@ update : Msg -> Model -> ( Model, Cmd Msg )
 update msg model =
     case msg of
         SetName word ->
-            ( model, Cmd.none )
+            ({ model | newPlayer = setNameIn word model.newPlayer }, Cmd.none)
 
         FetchPlayers data ->
-            ( model, Cmd.none )
+            case data of
+                Ok fetchedPlayers ->
+                    ({ model | players = fetchedPlayers, reqStatus = "" }, Cmd.none)
+
+                Err err ->
+                     ({ model | reqStatus = "An error has occurred!!!" }, Cmd.none)
 
         PostPlayerReq ->
-            ( model, Cmd.none )
+            ( model
+            , postPlayerReq model.baseUrl model.newPlayer
+            )
 
         AddPlayer data ->
-            ( model, Cmd.none )
+            let
+                _ =
+                    Debug.log "Data" data
+            in
+            case data of
+                Ok player ->
+                    ( { model | players = model.players ++ [player], newPlayer = initPlayer (player.id + 1) }
+                    , Cmd.none
+                    )
+
+                Err err ->
+                    ( { model | reqStatus = "An error has occurred!!!" }
+                    , Cmd.none
+                    )
 
         PutPlayerReq id status ->
-            ( model, Cmd.none )
+            case listLast <| List.filter (\player -> player.id == id) model.players of
+                Just playerToUpdate ->
+                    let
+                        updatedPlayer =
+                            { playerToUpdate | isActive = status }
+
+                    in
+                    ( model
+                    , putPlayerReq (model.baseUrl ++ String.fromInt id) updatedPlayer
+                    )
+
+                Nothing ->
+                    ( model, Cmd.none )
 
         ModifyPlayer data ->
-            ( model, Cmd.none )
+            case data of
+                Ok modifiedPlayer ->
+                    ( { model | players = List.map (\player -> if player.id == modifiedPlayer.id then modifiedPlayer else player) model.players }
+                    , Cmd.none
+                    )
+
+                Err err ->
+                    ( { model | reqStatus = "An error has occurred!!!" }
+                    , Cmd.none )
 
         DeletePlayerReq id ->
-            ( model, Cmd.none )
+            ( model
+            , deletePlayerReq model.baseUrl id
+            )
 
         DeletePlayer id data ->
-            ( model, Cmd.none )
+            case data of
+                Ok _ ->
+                    ( { model | players = List.filter (\player -> player.id /= id) model.players }
+                    , Cmd.none
+                    )
 
+                Err err ->
+                    ( { model | reqStatus = "An error has occurred!!!" }
+                    , Cmd.none
+                    )
+
+
+viewRequestStatus : String -> Html Msg
+viewRequestStatus status =
+    div [ id "request-status" ] [ text status ]
+
+setNameIn : String -> Player -> Player
+setNameIn name player =
+    { player | name = name }
 
 view : Model -> Html Msg
 view model =
-    h1 [] [ text "Elm Exercise: Players CRUD2" ]
+    div []
+        [ h1 [] [ text "Elm Exercise: Players CRUD" ]
+        , viewForm
+        , viewPlayers model.players
+        , viewRequestStatus model.reqStatus
+        ]
+
+viewForm : Html Msg
+viewForm =
+    form [ id "submit-player", onSubmit PostPlayerReq ]
+        [ input [ id "input-player", type_ "text", placeholder "Enter player name", onInput SetName ] []
+        , button [ id "btn-add", type_ "submit" ] [ text "Add Player" ]
+        ]
+
+viewPlayers : List Player -> Html Msg
+viewPlayers players =
+    ol [ id "players-list" ] (List.map viewPlayer players)
+
+viewPlayer : Player -> Html Msg
+viewPlayer player =
+    li [id <| "player-" ++ String.fromInt player.id]
+        [ div [class "player-name"] [text player.name]
+        , label [class "player-status"]
+            [ input
+                [ type_ "checkbox"
+                , class "player-status"
+                , onCheck (\isChecked -> PutPlayerReq player.id isChecked)
+                , Html.Attributes.checked player.isActive
+                ]
+                []
+            , text "Active"
+            ]
+        , button [class "btn-delete", onClick (DeletePlayerReq player.id)] [text "Delete"]
+        ]
+
 
 
 main : Program () Model Msg
